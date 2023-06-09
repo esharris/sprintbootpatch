f74df0f70846900e62303db1d1f1a80456c3a335
Earl Harris <esharris@mac.com>
2023-06-09 14:38:58 -0400

    Turned PatchDto into a record.

--- springbootpatch/src/main/java/com/jcg/controller/UserController.java
+++ springbootpatch/src/main/java/com/jcg/controller/UserController.java
@@ -1,11 +1,7 @@
 package com.jcg.controller;
 
-import com.jcg.dto.PatchDto;
-import com.jcg.exception.NotFoundEx;
-import com.jcg.exception.NotYetImplementedEx;
-import com.jcg.model.User;
-import com.jcg.service.UserService;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,33 +12,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jcg.dto.PatchDto;
+import com.jcg.exception.NotFoundEx;
+import com.jcg.exception.NotYetImplementedEx;
+import com.jcg.model.User;
+import com.jcg.service.UserService;
+
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-  @Autowired
-  UserService service;
+	@Autowired
+	UserService service;
 
-  @GetMapping("/all")
-  public ResponseEntity<List<User>> findAll() {
-    return ResponseEntity.ok(service.getAll());
-  }
+	@GetMapping("/all")
+	public ResponseEntity<List<User>> findAll() {
+		return ResponseEntity.ok(service.getAll());
+	}
 
-  @GetMapping("/{id}")
-  public ResponseEntity<User> findById(@PathVariable(name = "id") int id)
-      throws NotFoundEx {
-    return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
-  }
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable(name = "id") int id) throws NotFoundEx {
+		return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
+	}
 
-  @PatchMapping("/{id}")
-  public ResponseEntity<Boolean> updatePartially(@PathVariable(name = "id") int id,
-      @RequestBody PatchDto dto) throws NotYetImplementedEx, NotFoundEx {
-    // skipping validations for brevity
-    if (dto.getOp().equalsIgnoreCase("update")) {
-      boolean result = service.partialUpdate(id, dto.getKey(), dto.getValue());
-      return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
-    } else {
-      throw new NotYetImplementedEx("NOT_YET_IMPLEMENTED");
-    }
-  }
+	@PatchMapping("/{id}")
+	public ResponseEntity<Boolean> updatePartially(@PathVariable(name = "id") int id, @RequestBody PatchDto dto)
+			throws NotYetImplementedEx, NotFoundEx {
+		// skipping validations for brevity
+		if (dto.op().equalsIgnoreCase("update")) {
+			boolean result = service.partialUpdate(id, dto.key(), dto.value());
+			return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+		} else {
+			throw new NotYetImplementedEx("NOT_YET_IMPLEMENTED");
+		}
+	}
 }
--- springbootpatch/src/main/java/com/jcg/dto/PatchDto.java
+++ springbootpatch/src/main/java/com/jcg/dto/PatchDto.java
@@ -5,35 +5,5 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class PatchDto {
-
-	String op;
-
-	String key;
-
-	String value;
-
-	public String getOp() {
-		return op;
-	}
-
-	public void setOp(String op) {
-		this.op = op;
-	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
+public record PatchDto(String op, String key, String value) {
 }
