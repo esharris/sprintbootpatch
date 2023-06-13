95971465200b4424b0989e1ebddf07b48df257bd
Earl Harris <esharris@mac.com>
2023-06-13 11:50:11 -0400

    Added url to example link.

--- springbootpatch/src/main/java/com/jcg/SpringbootpatchApplication.java
+++ springbootpatch/src/main/java/com/jcg/SpringbootpatchApplication.java
@@ -3,6 +3,12 @@ package com.jcg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://examples.javacodegeeks.com/spring-boot-patch-request-example/
+ * 
+ * @author earlharris
+ *
+ */
 @SpringBootApplication
 public class SpringbootpatchApplication {
 
